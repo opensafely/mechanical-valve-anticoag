1c885e03384ccc66d27950adfa800881adc1f5f7
Brian <brianmkcebm@gmail.com>
2021-04-06 11:54:28 +0100

    amend url location again

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -13,7 +13,7 @@ doac_codes = codelist_from_csv(
 # https://github.com/opensafely/codelist-development/issues/91
 # The following is a placeholder but should be replaced by above issue
 mechanical_valve_codes = codelist_from_csv(
-    "codelists/opensafely-valvular-atrial-fibrillation.csv",
+    "codelists/opensafely-valvular-atrial-fibrillation-6cab4902.csv",
     system="ctv3",
     column="code",
 )
