90671314468f8381264eb374e3451c2ca6d48d03
Brian <brianmkcebm@gmail.com>
2021-04-06 14:59:49 +0100

    fix indexdate

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -54,7 +54,7 @@ study = StudyDefinition(
 
       doac=patients.with_these_medications(
         doac_codes,
-        between=["index date", "last_day_of_month(index_date)"],
+        between=["index_date", "last_day_of_month(index_date)"],
         return_expectations={"incidence": 0.2},
     ),
    
