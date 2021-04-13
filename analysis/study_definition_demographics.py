5105fd3eb513986864e0da0e7363ea2c7a6c4e9d
Brian <brianmkcebm@gmail.com>
2021-04-13 17:25:41 +0100

    add demographics

--- analysis/study_definition_demographics.py
+++ analysis/study_definition_demographics.py
@@ -7,7 +7,6 @@ from cohortextractor import (
 from codelists import *
 
 
-
 study = StudyDefinition(
     # Configure the expectations framework
     default_expectations={
@@ -17,62 +16,79 @@ study = StudyDefinition(
     },
     population=patients.satisfying(
         """registered 
-    	AND
+        AND
         (NOT has_died) 
         AND
         age>=16 AND age <= 120 
-        AND
-        mechanical_valve
         """,
-
-            registered=patients.registered_as_of(
-                "index_date", return_expectations={"incidence": 0.9},
-            ),
-            has_died=patients.died_from_any_cause(
-                on_or_before="index_date",
-                returning="binary_flag",
-                return_expectations={"incidence": 0.05},
-            ),
-            age=patients.age_as_of(
-                "index_date",
-                return_expectations={
-                    "rate": "universal",
-                    "int": {"distribution": "population_ages"},
-                },
-            ),
-            mechanical_valve=patients.with_these_clinical_events(
-                mechanical_valve_codes,
-                on_or_before="index_date",
-                returning="binary_flag",
-                return_expectations={"incidence": 0.01,},
-            ),
-  
-
-   ),
+        registered=patients.registered_as_of(
+            "index_date", return_expectations={"incidence": 0.9},
+        ),
+        has_died=patients.died_from_any_cause(
+            on_or_before="index_date",
+            returning="binary_flag",
+            return_expectations={"incidence": 0.05},
+        ),
+        age=patients.age_as_of(
+            "index_date",
+            return_expectations={
+                "rate": "universal",
+                "int": {"distribution": "population_ages"},
+            },
+        ),
+    ),
     index_date="2021-04-01",
-
-## Medication
-      doac=patients.with_these_medications(
+    ## Medication
+    doac=patients.with_these_medications(
         doac_codes,
         on_or_before="index_date - 3 months",
         return_expectations={"incidence": 0.2},
     ),
-
-
-## Mechanical Valve
-
-      mechanical_valve=patients.with_these_clinical_events(
-                mechanical_valve_codes,
-                on_or_before="index_date",
-                returning="binary_flag",
-                return_expectations={"incidence": 0.01,},
-            ),
-
-
-## Demographics
-
-
-
+    ## Mechanical Valve
+    mechanical_valve=patients.with_these_clinical_events(
+        mechanical_valve_codes,
+        on_or_before="index_date",
+        returning="binary_flag",
+        return_expectations={"incidence": 0.01,},
+    ),
+    ## Demographics
+    ##sex
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.5, "U": 0.01}},
+        }
+    ),
+    ##age
+    age_band=patients.categorised_as(
+        {
+            "0": "DEFAULT",
+            "0-19": """ age >= 0 AND age < 20""",
+            "20-29": """ age >=  20 AND age < 30""",
+            "30-39": """ age >=  30 AND age < 40""",
+            "40-49": """ age >=  40 AND age < 50""",
+            "50-59": """ age >=  50 AND age < 60""",
+            "60-69": """ age >=  60 AND age < 70""",
+            "70-79": """ age >=  70 AND age < 80""",
+            "80+": """ age >=  80 AND age < 120""",
+        },
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "0": 0.001,
+                    "0-19": 0.124,
+                    "20-29": 0.125,
+                    "30-39": 0.125,
+                    "40-49": 0.125,
+                    "50-59": 0.125,
+                    "60-69": 0.125,
+                    "70-79": 0.125,
+                    "80+": 0.125,
+                }
+            },
+        },
+    ),
     # Index of multiple deprivation
     imd=patients.address_as_of(
         "index_date",
@@ -89,8 +105,7 @@ study = StudyDefinition(
                 }
             },
         },
-    ), 
-
+    ),
     # Patients in long-stay nursing and residential care
     longres_dat=patients.with_these_clinical_events(
         codelists.longres,
@@ -99,7 +114,6 @@ study = StudyDefinition(
         on_or_before="index_date",
         date_format="YYYY-MM-DD",
     ),
-
     # Ethnicity
     eth2001=patients.with_these_clinical_events(
         codelists.eth2001,
@@ -154,7 +168,16 @@ study = StudyDefinition(
         on_or_before="index_date",
         date_format="YYYY-MM-DD",
     ),
-   
+    ##Organisation Demograhics
+    ##practice
+    practice=patients.registered_practice_as_of(
+        "index_date",
+        returning="pseudo_id",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 25, "stddev": 5},
+            "incidence": 0.5,
+        },
+    ),
     # stp is an NHS administration region based on geography
     stp=patients.registered_practice_as_of(
         "index_date",
@@ -178,4 +201,3 @@ study = StudyDefinition(
         },
     ),
 )
-
