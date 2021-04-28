bb7e727a51e59caec2c77c8ca020d225e933ffbf
LFISHER7 <lhf282@live.co.uk>
2021-04-28 14:52:56 +0100

    valve type counts notebook

--- analysis/get_patient_count.py
+++ analysis/get_patient_count.py
@@ -3,6 +3,8 @@ import json
 import pandas as pd
 import numpy as np
 
+patients_codes = {}
+doac_patients_codes = {}
 patients_list = []
 for file in os.listdir('output'):
         
@@ -14,9 +16,27 @@ for file in os.listdir('output'):
             patients = np.unique(df['patient_id'])
             patients_list.append(patients)
 
+
+            def get_patient_valve_code(row, output_dict):
+                patient = row['patient_id']
+                code = row['mechanical_valve_code']
+                output_dict[patient] = code
+            
+            df.apply(lambda row: get_patient_valve_code(row, patients_codes), axis=1)
+
+
+            doac_subset = df[df['doac']==1]
+            df.apply(lambda row: get_patient_valve_code(row, doac_patients_codes), axis=1)
+
             
 unique_patients = len(np.unique(patients_list))
 
 
 with open('output/patient_count.json', 'w') as f:
     json.dump({"num_patients": unique_patients}, f)
+
+with open('output/patient_valve_codes.json', 'w') as f:
+    json.dump({"num_patients": patients_codes}, f)
+
+with open('output/patient_doac_valve_codes.json', 'w') as f:
+    json.dump({"num_patients": doac_patients_codes}, f)
