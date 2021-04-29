9390f33e03b5b35b74253df2cb045c9910d2c717
LFISHER7 <lhf282@live.co.uk>
2021-04-29 12:46:06 +0100

    get recent doac mech count

--- analysis/get_patient_count.py
+++ analysis/get_patient_count.py
@@ -5,6 +5,8 @@ import numpy as np
 
 patients_codes = {}
 doac_patients_codes = {}
+recent_doac_patients_codes = {}
+
 patients_list = []
 for file in os.listdir('output'):
         
@@ -28,6 +30,11 @@ for file in os.listdir('output'):
             doac_subset = df[df['doac']==1]
             doac_subset.apply(lambda row: get_patient_valve_code(row, doac_patients_codes), axis=1)
 
+            # if last 3 months get separate count
+
+            if date in ['2021-04-01', '2021-03-01', '2021-02-01']:
+                doac_subset.apply(lambda row: get_patient_valve_code(row, recent_doac_patients_codes), axis=1)
+
             
 unique_patients = len(np.unique(patients_list))
 
@@ -40,3 +47,6 @@ with open('output/patient_valve_codes.json', 'w') as f:
 
 with open('output/patient_doac_valve_codes.json', 'w') as f:
     json.dump({"num_patients": doac_patients_codes}, f)
+
+with open('output/recent_patient_doac_valve_codes.json', 'w') as f:
+    json.dump({"num_patients": recent_doac_patients_codes}, f)
