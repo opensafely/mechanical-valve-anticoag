6ac4b130d70764b82dfab4e77e9a8b0232741276
LFISHER7 <louis.fisher@thedatalab.org>
2021-06-29 17:21:27 +0100

    move codelist

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -15,7 +15,7 @@ warfarin_codes = codelist_from_csv(
 )
 
 doac_codes = codelist_from_csv(
-    "codelists/opensafely-direct-acting-oral-anticoagulants-doac.csv",
+    "analysis/opensafely-direct-acting-oral-anticoagulants-doac.csv",
     system="snomed",
     column="id",
 )
