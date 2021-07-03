8d557fd0093e93cbe9da27cb5920d5463910f58c
LFISHER7 <louis.fisher@thedatalab.org>
2021-07-03 15:27:22 +0100

    restore af

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -135,12 +135,12 @@ study = StudyDefinition(
         }
     ),
     
-    # atrial_fib=patients.with_these_clinical_events(
-    #     af_codes,
-    #     on_or_before="index_date",
-    #     returning="binary_flag",
-    #     return_expectations={"incidence": 0.01,},
-    # ),
+    atrial_fib=patients.with_these_clinical_events(
+        af_codes,
+        on_or_before="index_date",
+        returning="binary_flag",
+        return_expectations={"incidence": 0.01,},
+    ),
     
 
     mechanical_valve=patients.with_these_clinical_events(
@@ -212,12 +212,12 @@ measures = [
         group_by="imd",
     ),
     
-    # Measure(
-    #     id="doac_rx_mechanical_valve_3_month_af_rate",
-    #     numerator="doac_3_months",
-    #     denominator="population",
-    #     group_by="atrial_fib",
-    # ),
+    Measure(
+        id="doac_rx_mechanical_valve_3_month_af_rate",
+        numerator="doac_3_months",
+        denominator="population",
+        group_by="atrial_fib",
+    ),
 
     Measure(
         id="doac_rx_mechanical_valve_3_month_ethnicity_rate",
