cde9dfd07674905e062d7ac9d50473c58b76883a
LFISHER7 <louis.fisher@thedatalab.org>
2021-07-05 10:08:05 +0100

    remove duplicate measures

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -218,13 +218,6 @@ measures = [
         denominator="population",
         group_by="atrial_fib",
     ),
-
-    Measure(
-        id="doac_rx_mechanical_valve_3_month_ethnicity_rate",
-        numerator="doac_3_months",
-        denominator="population",
-        group_by="imd",
-    ),
     
     Measure(
         id="doac_rx_mechanical_valve_3_month_ethnicity_rate",
